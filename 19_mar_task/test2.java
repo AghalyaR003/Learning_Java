879f5bf69f6f597362d92f2baa4973bad0522516
Aghalya <132271713+AghalyaR003@users.noreply.github.com>
2024-09-27 18:56:36 +0530

    Add files via upload

---
+++ 19_mar_task/test2.java
@@ -0,0 +1,14 @@
+class test2 
+{
+	public static void main(String[] args) 
+	{
+		int a=54;
+		char b=(char)a;
+		int c=(int)b;
+		double d=(double)c;
+		System.out.println("int:"+a);
+		System.out.println("char:"+b);
+		System.out.println("int:"+c);
+		System.out.println("double:"+d);
+	}
+}
---
+++ 19_mar_task/test3.java
@@ -0,0 +1,12 @@
+class test3 
+{
+	public static void main(String[] args) 
+	{
+		double a=28.5d;
+		int b=(int)a;
+		char c=(char)b;
+		System.out.println("double:"+a);
+		System.out.println("int:"+b);
+		System.out.println("char:"+c);
+	}
+}
